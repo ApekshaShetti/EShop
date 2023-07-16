b0b3f329073a55da0b5459974d44017a574b5a37
Apeksha <apekshashetti1012@gmail.com>
2023-07-16 13:33:31 +0530

    ref:testing

--- store/models/customer.py
+++ store/models/customer.py
@@ -4,7 +4,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50 )
     password = models.CharField(max_length=500)
 
 
