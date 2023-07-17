4afd1f76ae02bbff3f2ea499aa60b0d3f4d4bf8e
Apeksha <apekshashetti1012@gmail.com>
2023-07-17 20:05:50 +0530

    cart in progress

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index, login, signup
 
 urlpatterns = [
-    path('',index.index, name='homepage'), # calling the index function that is present in views.py to show the data when i route localhost:8000
+    path('',index.Index.as_view(), name='homepage'), # calling the index function that is present in views.py to show the data when i route localhost:8000
     path('signup',signup.SignUp.as_view(), name ='signup'), # calling the Signup class that is present in views.py to show the signup page when i route localhost:8000
     path('login',login.Login.as_view(), name = 'login')
 ]
--- store/views/index.py
+++ store/views/index.py
@@ -6,27 +6,43 @@ from store.models.customer import Customer # importing this to get the data of a
 from django.contrib.auth.hashers import make_password, check_password # for password hashing in admin panel
 from django.views import View
 
+# home page
+
+class Index(View):
+    def get(self,request):
+        all_products = None # when route this page it will call the function from products.py is present in models folder                                                                                                                                                                                                                        
+        all_categories = Category.get_all_categories() # when route this page it will call the function from category.py is present in models folder 
+        category_id = request.GET.get('category')
+        # if category id is passed then only it will show that products with that category id 
+        if category_id:
+            all_products = Product.get_all_products_by_category_id(category_id)
+        # if category id is not selected it will show all products 
+        else:
+            all_products = Product.get_all_products()
+        # data is a dictionary that will contain values of products, categories and any data that is to be shown in index.html 
+        data = {}
+        data['all_products'] = all_products 
+        data['all_categories'] = all_categories 
+        print('You are' , request.session.get('email'))
+        return render(request,'index.html',data)  # sending all data to index.html file
+    
 
 
-# Create your views here.
+    def post(self,request):
+        product = request.POST.get('product')
+        cart = request.session.get('cart')
+        if cart:
+            qty = cart.get(product)
+            if qty:
+                cart[product] = qty + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
 
-# home page 
-def index(request):   # function to show what should be there when i write localhost:8000 or home page
-    # return HttpResponse('<h1> Index Page</h1>')
-    all_products = None # when route this page it will call the function from products.py is present in models folder 
-    all_categories = Category.get_all_categories() # when route this page it will call the function from category.py is present in models folder 
-    category_id = request.GET.get('category')
-    # if category id is passed then only it will show that products with that category id 
-    if category_id:
-        all_products = Product.get_all_products_by_category_id(category_id)
-    # if category id is not selected it will show all products 
-    else:
-        all_products = Product.get_all_products()
-    # data is a dictionary that will contain values of products, categories and any data that is to be shown in index.html 
-    data = {}
-    data['all_products'] = all_products 
-    data['all_categories'] = all_categories 
-    print('You are' , request.session.get('email'))
-    return render(request,'index.html',data)  # sending all data to index.html file
+        request.session['cart'] = cart
+        print('cart', request.session['cart'])
+        return redirect('homepage')
 
-
+    
